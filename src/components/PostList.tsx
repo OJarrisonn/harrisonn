2d8cd2a351792a17f5eaa880b9ea1a2db05abecb
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-04-15 21:32:14 -0300

    Styling fixes

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -21,11 +21,11 @@ export default function PostList({ posts, term }: {posts: {
             }).sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime()).map((post) => (
                 <div key={post.id}>
                     <a href={`/blog/${post.slug}/`}>
-                        <img width={720} height={360} src={post.data.heroImage} alt="" />
-                        <div>
+                        <img width={720} height={360} src={post.data.heroImage} alt="" style={{marginBottom: "1rem"}}/>
+                        <div style={{color: "rgb(var(--gray-light))"}}>
                             <h4 className="title">{post.data.title}</h4>
                             <p>{post.data.description}</p>
-                            <p className="date">
+                            <p className="date" style={{color: "rgb(var(--gray))"}}>
                                 <FormattedDate date={post.data.pubDate} />
                             </p>
                         </div>
