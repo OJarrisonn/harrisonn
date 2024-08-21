40fac843e7fd216e0b770356f474f902bc59da14
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-08-21 19:54:54 -0300

    biome: configured biome

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
-import { Input } from "./ui/input";
 import PostList from "./PostList";
+import { Input } from "./ui/input";
 
-export default function Blog({posts}) {
-    const [term, setTerm] = useState("");
+export default function Blog({ posts }) {
+	const [term, setTerm] = useState("");
 
-    function handleOnSearch({ target = {} }) {
-        const { value } = target;
-        setTerm(value);
-    }
+	function handleOnSearch({ target = {} }) {
+		const { value } = target;
+		setTerm(value);
+	}
 
-    return <>
-            <div style={{marginBottom: 16}}>
-                <Input
-                    type="text"
-                    value={term}
-                    onChange={handleOnSearch}
-                    placeholder="Search posts"
-                    style={{borderRadius: 8}}
-                    />
-                
-            </div>
-			<PostList posts={posts} term={term}/>
-    </>;
+	return (
+		<>
+			<div style={{ marginBottom: 16 }}>
+				<Input
+					type="text"
+					value={term}
+					onChange={handleOnSearch}
+					placeholder="Search posts"
+					style={{ borderRadius: 8 }}
+				/>
+			</div>
+			<PostList posts={posts} term={term} />
+		</>
+	);
 }
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,37 +1,52 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 
-export default function PostList({ posts, term }: {posts: {
-    id: string;
-    slug: string;
-    body: string;
-    collection: string;
-    data: {
-        title: string;
-        description: string;
-        pubDate: Date;
-        updatedDate?: Date | undefined;
-        heroImage?: string | undefined;
-    };
-}[], term: string}) {
-    return <div className="grid grid-cols-2 gap-4">
-        {
-            posts.filter((value) => {
-                return value.data.title.toLowerCase().includes(term.toLowerCase());
-            }).sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime()).map((post) => (
-                <div key={post.id}>
-                    <a href={`/blog/${post.slug}/`}>
-                        <img width={720} height={360} src={post.data.heroImage} alt="" style={{marginBottom: "1rem"}}/>
-                        <div style={{color: "rgb(var(--gray-light))"}}>
-                            <h4 className="title">{post.data.title}</h4>
-                            <p>{post.data.description}</p>
-                            <p className="date" style={{color: "rgb(var(--gray))"}}>
-                                <FormattedDate date={post.data.pubDate} />
-                            </p>
-                        </div>
-                    </a>
-                </div>
-            ))
-        }
-    </div>;
+export default function PostList({
+	posts,
+	term,
+}: {
+	posts: {
+		id: string;
+		slug: string;
+		body: string;
+		collection: string;
+		data: {
+			title: string;
+			description: string;
+			pubDate: Date;
+			updatedDate?: Date | undefined;
+			heroImage?: string | undefined;
+		};
+	}[];
+	term: string;
+}) {
+	return (
+		<div className="grid grid-cols-2 gap-4">
+			{posts
+				.filter((value) => {
+					return value.data.title.toLowerCase().includes(term.toLowerCase());
+				})
+				.sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime())
+				.map((post) => (
+					<div key={post.id}>
+						<a href={`/blog/${post.slug}/`}>
+							<img
+								width={720}
+								height={360}
+								src={post.data.heroImage}
+								alt=""
+								style={{ marginBottom: "1rem" }}
+							/>
+							<div style={{ color: "rgb(var(--gray-light))" }}>
+								<h4 className="title">{post.data.title}</h4>
+								<p>{post.data.description}</p>
+								<p className="date" style={{ color: "rgb(var(--gray))" }}>
+									<FormattedDate date={post.data.pubDate} />
+								</p>
+							</div>
+						</a>
+					</div>
+				))}
+		</div>
+	);
 }
